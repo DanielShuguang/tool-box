24de46f260bde66dfe593e543dd3c139bb5ca074
DanielShuguang <41940314+DanielShuguang@users.noreply.github.com>
2023-07-31 09:14:47 +0800

    feat: 去除下载管理器

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -9,11 +9,6 @@ const routes: RouteRecordRaw[] = [
     path: '/readFile',
     component: () => import('@/views/ReadFile/ReadFile.vue'),
     meta: { title: '文件读取' }
-  },
-  {
-    path: '/downloadManager',
-    component: () => import('@/views/DownloadManager/DownloadManager.vue'),
-    meta: { title: '下载管理器' }
   }
 ]
 
