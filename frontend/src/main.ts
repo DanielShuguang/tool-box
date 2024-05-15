e8f6ea64130ea56f13859acb24dce986c7908b7f
DanielShuguang <41940314+DanielShuguang@users.noreply.github.com>
2024-05-15 11:27:57 +0800

    chore: 替换 tailwindcss

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
-import 'virtual:uno.css'
+import './assets/style/tailwind.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,13 +4,11 @@ import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
-import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    UnoCSS(),
     AutoImport({
       dts: './src/types/auto-imports.d.ts',
       imports: [
