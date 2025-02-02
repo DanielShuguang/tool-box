1db3ff20cb25879cb3a2a654511cbfaf2d135ebc
DanielShuguang <41940314+DanielShuguang@users.noreply.github.com>
2025-02-03 00:56:12 +0800

    feat(Windows激活): 优化激活过程和结果显示
    - 添加对 PowerShell 的支持，提高激活兼容性
    - 优化激活命令执行逻辑，增加错误处理
    - 改进激活信息检查方法，提高准确性
    - 增加非 Windows 系统提示和管理员权限提示
    - 优化界面布局和提示信息

--- src/views/WindowsActivatiion/WindowsActivatiion.vue
+++ src/views/WindowsActivatiion/WindowsActivatiion.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import dayjs from 'dayjs'
 import { Command } from '@tauri-apps/plugin-shell'
+import { getOS } from '@/utils/system'
 
 const message = useMessage()
 const activeState = useLocalStorage('windows-activation', 0)
@@ -9,7 +10,7 @@ const activeState = useLocalStorage('windows-activation', 0)
 const colors = ['text-red-500', 'text-yellow-500', 'text-green-500']
 
 const textColor = computed(() => {
-  const diff = dayjs().diff(dayjs.unix(activeState.value), 'days')
+  const diff = dayjs.unix(activeState.value).diff(dayjs(), 'days')
   // 剩余时间小于 60 天，则显示红色，小于 120 天，则显示黄色，否则显示绿色
   if (diff < 60) {
     return colors[0]
@@ -27,53 +28,72 @@ async function handleClick() {
 
 const activeCmds = [
   'slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43',
-  'slmgr /skms kms.03k.org',
+  'slmgr /skms kms.0t.net.cn',
   'slmgr /ato'
 ]
 
 async function handleActive() {
   for (const element of activeCmds) {
-    await Command.create('cscript', ['C:\\Windows\\System32\\slmgr.vbs', ...element.split(' ')], {
-      encoding: 'utf-8'
-    }).execute()
+    const script = `cscript //nologo C:/Windows/System32/slmgr.vbs ${element}`
+    const { stdout } = await Command.create('powershell', script, { encoding: 'gbk' }).execute()
+
+    const errors = ['错误', 'error']
+    if (errors.some(error => stdout.includes(error))) {
+      return message.error(`[激活失败] ${stdout}`)
+    }
   }
 }
 
 async function checkWindowsActiveInfo() {
   try {
     const { stdout } = await Command.create(
-      'cscript',
-      ['C:\\Windows\\System32\\slmgr.vbs', '/xpr'],
-      { encoding: 'utf-8' }
+      'powershell',
+      'cscript //nologo C:/Windows/System32/slmgr.vbs /xpr',
+      { encoding: 'gbk' }
     ).execute()
+    const errors = ['找不到产品密钥', 'error', 'not found']
+    if (errors.some(error => stdout.includes(error))) {
+      activeState.value = 0
+      return
+    }
     const dateReg = /\d{4}\/\d{1,2}\/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}/
     const expireDate = dateReg.exec(stdout)
     if (expireDate?.[0]) {
       activeState.value = dayjs(expireDate[0]).unix()
+    } else {
+      activeState.value = 1
     }
   } catch (error) {
-    message.error(`激活失败: ${error}`)
+    message.error(String(error))
   }
 }
 
+const isWindows = ref(false)
+
 onMounted(() => {
+  isWindows.value = getOS() === 'Windows'
   checkWindowsActiveInfo()
 })
 </script>
 
 <template>
   <div>
-    <n-space>
-      <n-text>
-        <span v-if="activeState">
-          <span :class="textColor">
-            {{ dayjs.unix(activeState).format('YYYY-MM-DD') }}
+    <n-alert v-if="!isWindows" type="warning">请在 Windows 下使用本功能</n-alert>
+    <template v-else>
+      <n-card>
+        <n-alert class="mb-[15px]" type="info">请确认本应用使用管理员权限打开</n-alert>
+        <n-text>
+          <span v-if="activeState > 1">
+            <span :class="textColor">
+              {{ dayjs.unix(activeState).format('YYYY-MM-DD') }}
+            </span>
+            到期
           </span>
-          到期
-        </span>
-        <span v-else>未激活</span>
-      </n-text>
-    </n-space>
-    <n-button type="primary" @click="handleClick">激活</n-button>
+          <span v-else-if="activeState === 1" class="text-green-500">已永久激活</span>
+          <span v-else class="text-red-500">未激活</span>
+        </n-text>
+      </n-card>
+      <n-button class="mt-[15px]" type="primary" @click="handleClick">激活</n-button>
+    </template>
   </div>
 </template>
