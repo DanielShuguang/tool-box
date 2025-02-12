6fa36f308adaf4db5a38daa010c4bfde1a92257d
胡曙光 <o_hushuguang@shizhuang-inc.com>
2025-02-12 14:47:34 +0800

    refactor(src-tauri): 更新 Windows 应用程序清单文件
    - 在 build.rs 文件中添加注释，提示开发者如何在打包时设置管理员权限
    - 优化 windows-app-manifest.xml 文件的格式，删除冗余注释

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -3,6 +3,7 @@ use tauri_build::{build, try_build, Attributes, WindowsAttributes};
 fn main() {
     // 判断系统类型
     if cfg!(windows) {
+        // 打包时需要管理员权限可在 manifest 文件中设置 level 为 requireAdministrator
         let windows =
             WindowsAttributes::new().app_manifest(include_str!("windows-app-manifest.xml"));
 
