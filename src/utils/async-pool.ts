60491d72b7e9f9dd878cf3e3695eacd34cefc042
胡曙光 <o_hushuguang@shizhuang-inc.com>
2025-07-18 16:30:54 +0800

    refactor(types): 优化类型导出并调整组件类型定义
    - 移除了 auto-imports.d.ts 中未使用的类型 Slot 和 Slots
    - 更新了 components.d.ts 文件的导出内容
    - 重构了 async-pool.ts 中的异步池逻辑，提高了代码可读性和健壮性

--- src/utils/async-pool.ts
+++ src/utils/async-pool.ts
@@ -10,7 +10,7 @@ export interface ListPool<Resp, Item> {
 }
 
 function isTotalPoolOption<R>(arg: any): arg is TotalPool<R> {
-  return 'list' in arg
+  return 'total' in arg
 }
 
 /**
@@ -22,7 +22,8 @@ export async function asyncPool<Resp>(opts: TotalPool<Resp>): Promise<Resp[]>
 export async function asyncPool<Resp, Item>(
   opts: TotalPool<Resp> | ListPool<Resp, Item>
 ): Promise<Resp[]> {
-  const { max = 5 } = opts
+  const { max: rawMax = 5 } = opts
+  const max = rawMax > 0 ? rawMax : 5
   let length = 0
   const isTotalOpt = isTotalPoolOption(opts)
   if (isTotalOpt) {
@@ -33,21 +34,24 @@ export async function asyncPool<Resp, Item>(
   }
 
   const resps: Resp[] = []
-  const fns: Array<() => Promise<any>> = []
-  for (let i = 0; i < length; i++) {
-    if (fns.length >= max) {
-      await Promise.race(fns.map(fn => fn()))
+  const runningTasks: Promise<void>[] = []
+
+  for (let i = 0; i < length; i += 1) {
+    if (runningTasks.length >= max) {
+      // 等待一个任务完成
+      await Promise.race(runningTasks)
     }
-    const fn = async () => {
+
+    const task = (async () => {
       const res = await (isTotalOpt ? opts.asyncFn(i) : opts.asyncFn(opts.list[i], i))
       resps.push(res)
-      const index = fns.indexOf(fn)
-      fns.splice(index, 1)
-    }
-    fns.push(fn)
+    })()
+
+    runningTasks.push(task)
   }
 
-  await Promise.all(fns.map(fn => fn()))
+  // 等待所有剩余任务完成
+  await Promise.all(runningTasks)
 
   return resps
 }
