c5dce3fba3b8c2d6dd6aae36d54d2a06fc1d7784
胡曙光 <o_hushuguang@shizhuang-inc.com>
2025-12-17 14:40:48 +0800

    build: 更新版本

--- src-tauri/src/utils/crypto.rs
+++ src-tauri/src/utils/crypto.rs
@@ -5,7 +5,7 @@ const USER_NAME: &str = "Daniel Hu";
 pub fn generate_app_key(content: &str) -> String {
     let mut hasher = Context::new();
     hasher.consume(USER_NAME.as_bytes());
-    let result = hasher.compute();
+    let result = hasher.finalize();
     let hash = format!("{:x}", result);
     format!("{}_{}", content, hash)
 }
