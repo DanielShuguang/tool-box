b2b60cd97dacdea62798cbd2cbeccbd8ebdd387b
Daniel Hu. 胡曙光 <1004317985@qq.com>
2026-01-27 23:01:00 +0800

    docs: 更新项目规则并添加类型检查脚本
    更新项目规则文档，明确代码质量检查流程
    在package.json中添加check脚本用于类型检查
    移除Accounting.vue中未使用的导入

--- src/views/Accounting/Accounting.vue
+++ src/views/Accounting/Accounting.vue
@@ -1,6 +1,5 @@
 <script setup lang="tsx">
 import { NTag, NButton, DataTableColumn } from 'naive-ui'
-import { useAccountingLogic } from './logic'
 import { AccountingRecord } from './types'
 import { useAccountingForm } from './hooks/useAccountingForm'
 import { useAccountingRecords } from './hooks/useAccountingRecords'
